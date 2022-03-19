959ab983d0606bcfc2e432ea75a9ac9bf8fe0cdb
rahul007-bit <yadavrahul8141@yahoo.com>
2022-03-19 13:29:26 +0530

    intial commit for website

---
+++ website/components/Landing.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const LandingPage = () => {
+  return <div>landingPage</div>;
+};
---
+++ website/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { LandingPage } from "../components/Landing";
+export default function Home() {
+  return <LandingPage />;
+}
