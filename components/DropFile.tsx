9d05ed412d1d57ae83a66b735b36d7f18cce6eba
Chirag <chiragsp@pop-os.localdomain>
2022-03-20 16:08:16 +0530

    completed progress bar nd file handling

--- components/DropFile.tsx
+++ components/DropFile.tsx
@@ -1,18 +1,31 @@
 import Head from 'next/head';
 import React, { useState } from 'react';
-
+import { useUploadForm } from './hooks';
 import Dropzone from 'react-dropzone';
-
+import { ProgressBar } from 'react-bootstrap';
 export const DropFile = () => {
   const fileTypes = ['Pcap'];
   const [file, setFile] = useState(null);
   const handleDrop = acceptedFiles => {
-    setFile(acceptedFiles.map(file => file.name));
+    // setFile(acceptedFiles.map(file => file.name));
+    setFile(file => acceptedFiles);
+    handleSubmit();
   };
+
+  const { isLoading, isSuccess, uploadForm, progress } = useUploadForm(
+    'http://localhost:5000/post',
+  );
+
+  const handleSubmit = async () => {
+    return await uploadForm(file);
+  };
+  // const now = 60;
 
   return (
     <div>
       <h3 className="_title1">Drop Your Pcap File Here</h3>
+      {isSuccess ? <p>sucess</p> : <p>fail</p>}
+      <ProgressBar now={progress} label={`${progress}%`} />
       <Dropzone
         onDrop={handleDrop}
         accept="image/*"
